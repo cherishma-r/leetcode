20f903ae2996915d7813560a641b366c16a56e60
cherishma-r <eng25am0259@dsu.edu.in>
2026-09-21 18:01:14 +0530

    Time: 1 ms (99.83%) | Memory: 46.5 MB (95.81%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,22 +1,18 @@
+import java.util.*;
+
 class Solution {
     public List<String> fizzBuzz(int n) {
-        
         List<String> answer = new ArrayList<>();
 
         for (int i = 1; i <= n; i++) {
-            
-            if (i % 15 == 0) {
+            if (i % 15 == 0)
                 answer.add("FizzBuzz");
-            }
-            else if (i % 3 == 0) {
+            else if (i % 3 == 0)
                 answer.add("Fizz");
-            }
-            else if (i % 5 == 0) {
+            else if (i % 5 == 0)
                 answer.add("Buzz");
-            }
-            else {
+            else
                 answer.add(String.valueOf(i));
-            }
         }
 
         return answer;
